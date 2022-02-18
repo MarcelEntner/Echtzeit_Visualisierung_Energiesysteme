f783113161fb0bc608ef33aa90d2ce7a17c20607
david.poechacker <david.poechacker@sz-ybbs.ac.at>
2022-02-18 10:35:16 +0100

    Überschrift

--- Energiesysteme/resources/views/HomePage.blade.php
+++ Energiesysteme/resources/views/HomePage.blade.php
@@ -44,38 +44,37 @@
         </div>
 
 
-        <!-- Beitrag -->
-        <div class="Beitrag shadow-lg rounded-3">
+           <!-- Beitrag -->
+           <div class="Beitrag shadow-lg rounded-3">
 
-            <h3 style="padding:10px;font-family: Arial, sans-serif;" class="text-center"> <b> Über uns</b></h3> <!-- Überschrift -->
+            <h3 style="font-family: Arial;" class="text-center"> <b>Intelligente Strom- und Mikronetze</b></h3> <!-- Überschrift -->
 
             <!-- Text -->
             <p class="text">
-                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore
-                et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
-                Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit
-                amet,
-                consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-                sed
-                diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
-                takimata
-                sanctus est Lorem ipsum dolor sit amet.
-                et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
-                Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit
-                amet,
-                consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-                sed
-                diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
-                takimata
-                sanctus est Lorem ipsum dolor sit amet.
-                et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
-                Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit
-                amet,
-                consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-                sed
-                diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
-                takimata
-                sanctus est Lorem ipsum dolor sit amet.
+           
+
+Die 2017 neu gegründete Area „Smart- und Microgrids“ beschäftigt sich mit der konzeptionellen Planung und Steuerung von dezentralen Energieversorgungsprojekten und Microgrids. Dazu werden theoretische, physikalische und wissenschaftliche Zusammenhänge im Bereich der Planung und Steuerung von Smart- und Microgrids erforscht und experimentell entwickelt. Die angewandten Methoden inkludieren Mixed Integer Linear Programming (MILP) oder linearisierte MILP sowie Model-Predictive-Control(MPC)-Methoden. Eine ganzheitliche Betrachtung (Konzeptionierung und Betrieb) von Multi-Energiesystemen (Strom, Wärme, Kälte für Gebäude sowie für kommunale und industrielle Anwendungen) stellt eine komplexe Herausforderung dar, die nur durch den Einsatz der genannten Methoden nachhaltig lösbar ist.
+
+Das Hauptziel besteht darin, den Grad der Autonomie der Systeme auf allen hierarchischen Ebenen (einzelne Gebäude, Siedlungen, Energiegemeinschaften, Subnetze, Regionen) und in allen Sektoren des Energiesystems zu erhöhen. Dadurch steigert sich die Gesamteffizienz nachhaltig und die überregionale Infrastruktur wird entlastet. Die Arbeiten dienen als Grundlage für die Entwicklung von Werkzeugen, von welchen das gesamte Energiesystem langfristig profitieren soll.
+            </p>
+
+            <h3 style="padding:10px; font-family: Arial;" class="text-center"> <b>Entwicklungsfelder und Anwendungsgebiete </b></h3> <!-- Überschrift -->
+
+            <!-- Text -->
+            <p class="text">
+                » Entwicklung von Methoden und Tools für die Energiesystemplanung und die sektorenübergreifende Energieinfrastruktur sowie die Identifizierung von Schwachstellen
+
+                » Schaffung einer prädiktiven Steuerung via MPC, die die Wechselwirkungen zwischen den Sektoren berücksichtigt und durch den Einsatz von Speichern und Lastverschiebungen mehr Flexibilität und Stabilität im Energiesystem bietet
+            </p>
+
+
+            <h3 style="padding:10px; font-family: Arial;" class="text-center"> <b> Forschungsprojekt Microgrid Lab </b></h3> <!-- Überschrift -->
+
+            <!-- Text -->
+            <p class="text">
+                In dem Forschungsprojekt Microgrid Lab wird ein Microgrid für kommunale Energiekonzepte in einem realen Umfeld geplant, errichtet, evaluiert und auf wissenschaftlicher Ebene weiterentwickelt. Ziel ist die Etablierung des Forschungslabors für verschiedene Wirtschaftszweige, um Planungs-, Steuerungs-, Integrations- und Kommunikationskonzepte zu entwickeln und für den Markt zu testen. Das betrifft auch sektorenübergreifende Energienetze (Wärme, Strom, Gas, Wasserstoff). Projektinhalte sind die wissenschaftliche Planung, die Inbetriebnahme, ein standardisiertes Monitoring der Verbraucher/Erzeuger (u.a. Biomasse, PV, Batterie, E-Ladestationen, Absorptionskälte), die Entwicklung von Testzyklen, die Weiterentwicklung der Optimierungsalgorithmen und ein Wissenstransfer zu verschiedenen Stakeholdergruppen. Mithilfe entwickelter mathematischer Methoden wurde bereits ein optimales Energieplanungskonzept entwickelt, welches auf Gemeinden übertragen werden kann. Die innovativen Planungs- und Steuerungskonzepte ermöglichen CO2- und Kosteneinsparungen von bis zu 90% bzw. 40%.
+
+
             </p>
         </div>
     </body>
